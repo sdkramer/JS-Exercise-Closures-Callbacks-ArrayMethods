64534f39fc87a7336d12a9f66e30de73272bf1de
Sean Kramer <seandominickramer@gmail.com>
2020-03-24 18:04:13 -0400

    Add correct solution to

--- index.js
+++ index.js
@@ -253,8 +253,12 @@ return previous + current;
  * @returns an array with all the runners' full names in the following format: "Smith, John".
  * The full names appear in the array in the same order the runners appear in the `runners` array.
 */
-function getFullNames(/* CODE HERE */) {
-  /* CODE HERE */
+function getFullNames(runners) {
+  let list = [];
+  runners.forEach(function(item){
+    return list.push(`${item.last_name}, ${item.first_name}`)
+  });
+  return list;
 }
 
 /**
