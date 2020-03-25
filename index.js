85a2a626e534b18dbb19a62288783b268b9c53e0
Sean Kramer <seandominickramer@gmail.com>
2020-03-24 20:34:38 -0400

    Add solution to

--- index.js
+++ index.js
@@ -294,8 +294,11 @@ function firstNamesAllCaps(runners) {
  * @returns an array containing only the runners that use the given `tShirtSize`.
  * The runners in the array appear in the same order they appear in the `runners` array.
 */
-function getRunnersByTShirtSize(runners) {
-  
+function getRunnersByTShirtSize(runners, tShirtSize) {
+let size = runners.filter(function(item){
+  return item.shirt_size == tShirtSize;
+});
+return size;
   
 }
 
